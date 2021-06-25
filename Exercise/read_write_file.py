7a6692bd2a5d6243592901d9a213a9abb4545869
Teng kimhan <tengkimhan@gmail.com>
2021-06-25 20:12:31 +0700

    read write file

--- Exercise/read_write_file.py
+++ Exercise/read_write_file.py
@@ -1,4 +1,3 @@
-
 # 1. poem.txt contains famous poem "Road not taken" by poet Robert Frost.
 #    You have to read this file in your python program and find out words with maximum occurance.
 import re
@@ -6,43 +5,39 @@ import re
 word_value = {}
 
 with open("poem.txt", "r") as f:
+    i = 0
     for line in f:
-        line_split = re.split("\s|[.,:—!:;]", line)
-        for word in line_split:
+        word_split = re.split(pattern=" |[.,:—!:;]", string=line.strip()) # strip() new line
+        i+=1
+        print("line", i)
+
+        print("Before strip:", line)
+        print("After strip:", line.strip()) # strip() newline character
+
+        print("Before remove empty string:", word_split)
+
+        # remove empty string after split word
+        while "" in word_split:
+            word_split.remove("")
+        print("After remove empty string:", word_split)
+
+        # add word and its occurance into dictionary
+        for word in word_split:
             if word in word_value:
-                word_value[word] +=1
+                word_value[word] += 1
             else:
                 word_value[word] = 1
 
-word_occurance = list(word_value.values())
-max = max(word_occurance)
+# print key value pair of the dictionary
+for key, val in word_value.items():
+    print(f"{key} {val}")
+
+print("Max value:", max(list(word_value.values())))
+max = max(list(word_value.values())) # maximum value of occurance word
+
 for key, value in word_value.items():
     if value == max:
-        print(f"{key} {value}")
-
-
-# word_stats={}
-#
-# with open("poem.txt","r") as f:
-#     for line in f:
-#       words=line.split(' ')
-#       for word in words:
-#         if word in word_stats:
-#           word_stats[word]+=1
-#         else:
-#           word_stats[word] = 1
-#
-# print(word_stats)
-#
-# word_occurances = list(word_stats.values())
-# max_count = max(word_occurances)
-# print("Max occurances of any word is:",max_count)
-#
-# print("Words with max occurances are: ")
-# for word, count in word_stats.items():
-#     if count==max_count:
-#         print(word)
-
+        print(f"Max key and value: {key} {value}")
 
 # 2. stocks.csv contains stock price, earnings per share and book value.
 #    You are writing a stock market application that will process this file
